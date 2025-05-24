97e5f8ba85bbb953969a8683ec2b3e3da6c9cc62
Jai Soni <jaiusoni2003@gmail.com>
2025-05-25 00:52:04 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Rooftop Analyzer',
+  description: 'AI-Powered Solar Rooftop Analyzer',
+  generator: 'JAI SONI',
 }
 
 export default function RootLayout({
